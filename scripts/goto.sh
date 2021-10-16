301a68de3327cc9be49d2bd22ce8e077ccc3fb03
João Pedro <jpedro@gmail.com>
2021-10-15 23:22:56 -0300

    New locations for goto script

--- scripts/goto.sh
+++ scripts/goto.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-code_folders=`echo ~/code/*/**/ | tr ' ' '\n'`
+code_folders=`echo ~/code/*/**/ ~/code/* | tr ' ' '\n'`
 dotfiles=`echo ~/.dotfiles ~/.dotfiles/*/| tr ' ' '\n'`
-configs=`echo ~/.config/nvim`
-work_folders=`echo ~/Fis-21-2/*/**/ ~/Fis-21-2/*/ ~/Documentos/*/**/ ~/Documentos/ |tr ' ' '\n'`
+configs=`echo ~/.config/* ~/config/*/**/ | tr ' ' '\n'`
+work_folders=`echo ~/Fis-21-2/*/**/ ~/Fis-21-2/*/ ~/Documentos/*/**/ ~/Documentos/ ~/Estagio-II ~/Estagio-II/* |tr ' ' '\n'`
 
 selected=`printf "$code_folders\n$dotfiles\n$configs\n$work_folders" | fzf`
 
