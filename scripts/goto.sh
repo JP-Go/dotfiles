d48807a57128286bd0bdc489a65f5a1cda4ad10d
João Pedro <jpedro@gmail.com>
2021-11-23 10:58:26 -0300

    Show hidden files in the search

--- scripts/goto.sh
+++ scripts/goto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-selected=$(fd . /home/jp/ -t d -t l --color never| fzf)
+selected=$(fd . /home/jp/ -t d -t l --color never --hidden | fzf)
 
 if [[ $selected != "" ]]; then
    cd $selected 
