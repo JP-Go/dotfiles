d3f2ff1a4411d04d171df7f4afe6d0a7acc205f2
João Pedro <jpedro@gmail.com>
2021-12-13 23:38:01 -0300

    Change script to create SR tex model

--- scripts/novosr.sh
+++ scripts/novosr.sh
@@ -1,1 +1,4 @@
-cp ~/SR/resources/modelo-sr.tex $"./$1.tex"
+touch $"./$1.tex"
+echo "\input{~/SR/resources/modelo-sr.tex}" >> $"./$1.tex"
+echo "\begin{document}" >> $"./$1.tex"
+echo "\end{document}" >> $"./$1.tex"
