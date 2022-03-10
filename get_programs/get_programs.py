7bd328a44e1370e3c859483e12d66e20a32de78d
João Pedro <jpedro@gmail.com>
2022-03-10 12:49:18 -0300

    Implement a confirmation prompr for the user's choices

--- get_programs/get_programs.py
+++ get_programs/get_programs.py
@@ -2,44 +2,70 @@
 
 from os import EX_USAGE
 import sys
-from typing import Sequence
+from input_handlers import get_answer
+from typing import Sequence, Union
 from constants import DE_OR_WM_BUNDLES, TERMINAL_EMULATORS
 from pickers import pick_program, pick_among_programs, pick_among_bundles
 
+packages = Union[Sequence[str], str, dict[str, list[str]]]
 
-def enumerate_options(options: Sequence[str] | dict[str, list[str]]):
-    if isinstance(options, dict):
-        options = list(options.keys())
-    for i, option in enumerate(options):
+
+def enumerate_options(packages: Sequence[str] | dict[str, list[str]]):
+    if isinstance(packages, dict):
+        packages = list(packages.keys())
+    for i, option in enumerate(packages):
         print(f"{i}: {option}")
-    if not isinstance(options, dict):
-        print(f"Default is {options[0]}")
+    if not isinstance(packages, dict):
+        print(f"Default is {packages[0]}")
         return
-    print(f"Default is {list(options.keys())[0]}")
+    print(f"Default is {list(packages.keys())[0]}")
+
+
+def present_packages(prompt: str, packages: packages):
+    print(prompt)
+    enumerate_options(packages)
+
+
+def confirm_coiches(packages: list[str | None]) -> bool | None:
+    print(f"Are these the packages you want to install? {packages} ?")
+    result = get_answer()
+    return result
+
+
+def install_packages(command, packages):
+    print(f" Installing {','.join(str (x) for x in packages)} with {command}")
 
 
 if __name__ == "__main__":
-    if not len(sys.argv) > 1:
-        print("[ERROR]: Incorrect Usage")
-        print("usage: ./get_programs.py <package_manager>")
-        exit(EX_USAGE)
+    while True:
+        picks = []
+        if not len(sys.argv) > 1:
+            print("[ERROR]: Incorrect Usage")
+            print("usage: ./get_programs.py <package_manager>")
+            exit(EX_USAGE)
 
-    print("Which terminal emulator would you like?")
-    enumerate_options(TERMINAL_EMULATORS)
-    picks = pick_among_programs(TERMINAL_EMULATORS)
-    print()
+        present_packages("Which terminal emulator would you like?", TERMINAL_EMULATORS)
+        picks = pick_among_programs(TERMINAL_EMULATORS, picks)
+        print()
 
-    print("Would you like to use zsh?")
-    picks = pick_program("zsh", picks)
-    print()
+        print("Would you like to use zsh?")
+        picks = pick_program("zsh", picks)
+        print()
 
-    print("Which DE or WM would you like to use? ")
-    enumerate_options(DE_OR_WM_BUNDLES)
-    picks = pick_among_bundles(DE_OR_WM_BUNDLES, picks)
-    print()
+        present_packages("Which DE or WM would you like to use? ", DE_OR_WM_BUNDLES)
+        picks = pick_among_bundles(DE_OR_WM_BUNDLES, picks)
+        print()
 
-    print("Would you like to use polybar?")
-    picks = pick_program("polybar", picks)
-    print()
+        print("Would you like to use polybar?")
+        picks = pick_program("polybar", picks)
+        print()
 
-    print(picks)
+        print(f"Are these the packages you want to install? {picks} ?")
+        confirmed = get_answer()
+        if confirmed:
+            install_packages("yay", picks)
+            break
+        print("Do you want to star from the beginning")
+        wants_redo = get_answer()
+        if not wants_redo:
+            exit(0)
