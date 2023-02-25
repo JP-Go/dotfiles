577c80534c529b0811827814cf9fdf32a9329e9a
João Pedro <jpedro1580@gmail.com>
2023-02-25 18:02:57 -0300

    Simplify enviroment build

--- bash/alias.bash
+++ bash/alias.bash
@@ -1,46 +1,13 @@
 #vim: ft=bash
+#
+alias vim="nvim"
+alias v="nvim"
 
-alias tlmgr="${TEXMFDIST}/scripts/texlive/tlmgr.pl --usermode"
-
-if [[ -f /usr/bin/doas ]]; then alias sudo="doas"; fi
-if [[ -f /usr/bin/exa ]]; then
-    alias l="exa --icons" 
-    alias ls="exa --icons" 
-    alias la="exa -lhaG --icons"
-    alias ll="exa -lhG --icons"
-fi
-
-if [[ -f /usr/bin/lsd ]]; then
-    alias l="lsd"
-    alias ls="lsd"
-    alias ll="lsd -l" 
-    alias la="lsd -a"
-fi
-
-if [[ -f ~/.local/nvim/bin/nvim ]]; then
-    alias nvim="~/.local/nvim/bin/nvim"
-    alias vim="~/.local/nvim/bin/nvim"
-    alias v="~/.local/nvim/bin/nvim"
-fi
-
-if [[ -f /usr/bin/nvim ]]; then
-    alias vim="/usr/bin/nvim"
-    alias v="/usr/bin/nvim"
-fi
-
-if [[ -f /usr/bin/gs ]]; then 
-    alias gita="git add"
-    alias gitc="git commit"
-    alias gitd="git diff"
-    alias gits="git status"
-    alias gitp="git push"
-else
-    alias ga="git add"
-    alias gc="git commit"
-    alias gd="git diff"
-    alias gs="git status"
-    alias gp="git push"
-fi
+alias ga="git add"
+alias gc="git commit"
+alias gd="git diff"
+alias gs="git status"
+alias gp="git push"
 
 if [[ -f /usr/bin/lazygit ]]; then alias lg="lazygit" ; fi
 if [[ -f /usr/bin/btop ]]; then alias htop="btop" ; fi
--- link.sh
+++ link.sh
@@ -11,45 +11,6 @@ ln -sf $PWD/kitty ~/.config/
 ln -sf  $PWD/.gitconfig ~/.gitconfig
 ln -sf $PWD/starship.toml ~/.config/starship.toml
 ln -sf $PWD/wallpapers ~/Imagens/
-
 ln -sf $PWD/stylua.toml ~/.config/stylua.toml
-
-if [[ -f /usr/bin/bspwm ]];then 
-  if ! [[ -d ~/.config/bspwm/ ]];then mkdir -p ~/.config/bspwm; fi
-  if ! [[ -d ~/.config/sxhkd/ ]];then mkdir -p ~/.config/sxhkd; fi
-  ln -sf $PWD/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
-  ln -sf $PWD/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
-fi
-
-if [[ -f /usr/bin/polybar ]];then 
-	ln -sf $PWD/polybar ~/.config/
-fi
-
-# Config files for bash since most Linux systems use it
-if [[ $(printenv SHELL | grep "bash") ]]; then
-	mkdir -p ~/.config/bash
-	ln -sf $PWD/bash/.bashrc ~/.bashrc
-	ln -sf $PWD/bash/alias.bash ~/.config/bash/alias.bash
-	ln -sf $PWD/bash/exports.bash ~/.config/bash/exports.bash
-	ln -sf $PWD/bash/completions-himalaya.bash ~/.config/bash/completions-himalaya.bash
-fi
-
-if [[ $(printenv SHELL | grep "zsh") ]]; then
-	mkdir -p ~/.config/zsh/
-	ln -sf $PWD/zsh/zshrc ~/.zshrc
-	ln -sf $PWD/zsh/aliases.zsh		~/.config/zsh/aliases.zsh
-	ln -sf $PWD/zsh/exports.zsh		~/.config/zsh/exports.zsh
-	ln -sf $PWD/zsh/functions.zsh	~/.config/zsh/functions.zsh
-	ln -sf $PWD/zsh/prompt.zsh		~/.config/zsh/prompt.zsh
-	ln -sf $PWD/zsh/vim-mode.zsh	~/.config/zsh/vim-mode.zsh
-fi
-
-if [[ -f /usr/bin/i3 ]]; then
-	mkdir -p ~/.config/i3/
-	ln -sf $PWD/i3/config			~/.config/i3/config
-	ln -sf $PWD/i3/config.keycodes  ~/.config/i3/config.keycodes
-	ln -sf $PWD/i3/i3status.conf    ~/.config/i3/i3status.conf
-fi
-
-ln -sf $PWD/rofi ~/.config/
+ln -sf $PWD/i3	~/.config/
 ln -sf $PWD/helix ~/.config/
