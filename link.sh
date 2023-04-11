53a4dca39fbff74038fab65499b5d6ace3c64721
João Pedro <jpedro1580@gmail.com>
2023-04-11 19:46:05 -0300

    Formatting and adding again the bashrc

--- link.sh
+++ link.sh
@@ -3,14 +3,16 @@
 # respective config file.
 set -x
 
-PWD=`pwd`
+PWD=$(pwd)
 
 # Basic workflow packages
 ln -sf $PWD/alacritty ~/.config/
 ln -sf $PWD/kitty ~/.config/
-ln -sf  $PWD/.gitconfig ~/.gitconfig
+ln -sf $PWD/.gitconfig ~/.gitconfig
 ln -sf $PWD/starship.toml ~/.config/starship.toml
 ln -sf $PWD/wallpapers ~/Imagens/
 ln -sf $PWD/stylua.toml ~/.config/stylua.toml
-ln -sf $PWD/i3	~/.config/
+ln -sf $PWD/i3 ~/.config/
 ln -sf $PWD/helix ~/.config/
+ln -sf $PWD/bash/.bashrc ~/.bashrc
+ln -sf $PWD/bash ~/.config/
