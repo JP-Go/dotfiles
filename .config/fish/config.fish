6fd408c5d3cded4a76bc92552d4fcbc8be165f20
João Pedro <jpedro1580@gmail.com>
2024-10-08 13:25:49 -0300

    refactor(fish): same as bash

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,44 +3,23 @@ if status is-interactive
 end
 
 # Variables
-if test -f ~/.local/bin/lvim 
-  set EDITOR lvim
-  set VISUAL lvim
-else 
-  set EDITOR nvim
-  set VISUAL nvim
-end
-export EDITOR
-export VISUAL
 
-set TEXMFDIST /usr/share/texlive/texmf-dist
-set TEXMFLOCAL /usr/share/texlive/texmf-local:/usr/share/texmf
-set TEXMFSYSVAR /var/lib/texmf
-set TEXMFSYSCONFIG /etc/texlive/
-set TEXMFHOME ~/texmf
-set TEXMFVAR ~/.texlive/texmf-var
-set TEXMFCONFIG ~/.texlive/texmf-config
-
-set PATH $PATH /home/jp/.cargo/bin
+set PATH $PATH $HOME/.cargo/bin
+set PATH $PATH $HOME/go/bin
 set PATH $PATH $HOME/.cargo/env
 set PATH $PATH $HOME/.local/bin
-set PATH $PATH $HOME/.local/share/nvim/mason/bin
 
 
 # Abbreviations
 
 abbr vim $EDITOR
 abbr lg lazygit
-abbr ga git add
+abbr ga git add --patch
 abbr gc git commit
 abbr gd git diff
 abbr gs git status
 abbr gp git push
 
-if test -f /usr/bin/btop
-    alias htop btop
-end
-
-if test -f ~/.sdkman/bin/sdkman-init.sh
-    bash ~/.sdkman/bin/sdkman-init.sh
+if test -f ~/.config/envman/load.sh
+    bash ~/.config/envman/load.sh
 end
