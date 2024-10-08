f4a88f47657c14463b323879bfd828464cc4edc8
João Pedro <jpedro1580@gmail.com>
2024-10-08 13:25:36 -0300

    refactor(bash): remove unused variables

--- .config/bash/exports.bash
+++ .config/bash/exports.bash
@@ -15,26 +15,4 @@ export TEXMFHOME=~/texmf
 export TEXMFVAR=~/.texlive/texmf-var
 export TEXMFCONFIG=~/.texlive/texmf-config
 
-# Add perl scripts, ruby gems and cargo packages(rust) to PATH
-PATH=$PATH:/home/jp/.cargo/bin:/home/jp/.local/share/gem/ruby/3.0.0/bin
-PATH="/home/jp/perl5/bin${PATH:+:${PATH}}"; 
-if [[ -f $HOME/.cargo/env ]]; then . "$HOME/.cargo/env";fi
-# Add yarn or npm downloaded binaries to path
-if [[ -d $HOME/.local/bin ]]; then PATH="$PATH:$HOME/.local/bin";fi
-# Add some perl env variables
-PERL5LIB="/home/jp/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
-PERL_LOCAL_LIB_ROOT="/home/jp/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
-PERL_MB_OPT="--install_base \"/home/jp/perl5\""; export PERL_MB_OPT;
-PERL_MM_OPT="INSTALL_BASE=/home/jp/perl5"; export PERL_MM_OPT;
-
-# Add C libs in usr/local/lib
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
-export NNN_TMPFILE=${HOME}/.config/nnn/.lastd
-
 PATH=${PATH}:${HOME}/.local/bin
-# export ANDROID_HOME=~/Android/Sdk
-# PATH=$PATH:$ANDROID_HOME/emulator
-# PATH=$PATH:$ANDROID_HOME/tools
-# PATH=$PATH:$ANDROID_HOME/tools/bin
-# PATH=$PATH:$ANDROID_HOME/platform-tools
-# PATH=$PATH:$ANDROID_HOME/../android-studio/bin
