c3fb7debb431bce760d33b1a9136883b8317a8a6
João Pedro <jpedro1580@gmail.com>
2024-10-21 10:01:51 -0300

    feat(fish): add deno config

---
+++ .config/fish/conf.d/deno.fish
@@ -0,0 +1,1 @@
+source "/home/jp/.deno/env.fish"
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,8 +7,13 @@ end
 set PATH $PATH $HOME/.cargo/bin
 set PATH $PATH $HOME/go/bin
 set PATH $PATH $HOME/.cargo/env
-set PATH $PATH $HOME/.local/bin
+set -x PATH $PATH $HOME/.local/bin
 
+if type -q nvim
+    set EDITOR nvim
+end
+
+zoxide init fish | source
 
 # Abbreviations
 
@@ -19,7 +24,3 @@ abbr gc git commit
 abbr gd git diff
 abbr gs git status
 abbr gp git push
-
-if test -f ~/.config/envman/load.sh
-    bash ~/.config/envman/load.sh
-end
