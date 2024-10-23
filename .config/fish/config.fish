11c5c66ff4abfedbc10bd52fb34f5efd02a8c906
João Pedro <jpedro1580@gmail.com>
2024-10-23 09:17:08 -0300

    feat(fish): config fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,9 @@ end
 set PATH $PATH $HOME/.cargo/bin
 set PATH $PATH $HOME/go/bin
 set PATH $PATH $HOME/.cargo/env
+set PATH $PATH $HOME/.local/opt/node/bin
+set PATH $PATH $HOME/.local/opt/node/bin
+set PATH $PATH $HOME/.local/share/pnpm/
 set -x PATH $PATH $HOME/.local/bin
 
 if type -q nvim
