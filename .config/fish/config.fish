3c6930b46d1bdd972992bcfeec3f3ead02e75841
João Pedro <jpedro1580@gmail.com>
2025-01-29 19:12:08 -0300

    feat(fish): add data grig,go binaries and pnpm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,6 +13,8 @@ set PATH $PATH $HOME/.cargo/env
 set PATH $PATH $HOME/.local/opt/node/bin
 set PATH $PATH $HOME/.local/opt/node/bin
 set PATH $PATH $HOME/.local/share/pnpm/
+set PATH $PATH /usr/local/go/bin
+set PATH $PATH /opt/DataGrip/bin/
 set -x PATH $PATH $HOME/.local/bin
 
 if type -q nvim
@@ -34,3 +36,10 @@ abbr gp git push
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# pnpm
+set -gx PNPM_HOME "/home/jp/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
