d5ce802c898893801400d998b8a80eb32d80b5cc
João Pedro <jpedro1580@gmail.com>
2025-08-13 09:20:56 -0300

    feat(android): android paths

---
+++ .config/fish/conf.d/android.fish
@@ -0,0 +1,5 @@
+set ANDROID_HOME $HOME/code/sdks/Android/sdk
+set PATH $PATH $ANDROID_HOME/tools
+set PATH $PATH $ANDROID_HOME/tools/bin
+set PATH $PATH $ANDROID_HOME/platform-tools
+set PATH $PATH $HOME/code/sdks/Android/cmdline-tools/latest/bin/
